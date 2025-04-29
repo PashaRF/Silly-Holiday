4670f8f13ac471a30d86cc0f7ce84273c7c03c24
pashrusi523 <pashrusi523@SDS-L-262PZH>
2025-04-29 14:40:23 -0400

    Adding Front of Card

--- Silly Holiday/Form1.Designer.cs
+++ Silly Holiday/Form1.Designer.cs
@@ -28,27 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cardButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // cardButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(400, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(327, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.cardButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.cardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.8F);
+            this.cardButton.ForeColor = System.Drawing.Color.Linen;
+            this.cardButton.Location = new System.Drawing.Point(30, 355);
+            this.cardButton.Name = "cardButton";
+            this.cardButton.Size = new System.Drawing.Size(416, 149);
+            this.cardButton.TabIndex = 0;
+            this.cardButton.Text = "Open Me!!";
+            this.cardButton.UseVisualStyleBackColor = false;
+            this.cardButton.Click += new System.EventHandler(this.cardButton_Click);
             // 
             // Form1
             // 
@@ -56,21 +51,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(482, 553);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cardButton);
             this.ForeColor = System.Drawing.Color.DarkGray;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Holiday Card";
+            this.Text = "Cheery Card";
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cardButton;
     }
 }
 
--- Silly Holiday/Form1.cs
+++ Silly Holiday/Form1.cs
@@ -10,11 +10,13 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Windows.Forms;
 using static System.Windows.Forms.AxHost;
+using System.Media;
 
 namespace Silly_Holiday
 {
     public partial class Form1 : Form
     {
+        #region Variables
         int startx = 50;
         int starty = 50;
         int length = 20;
@@ -31,31 +33,33 @@ namespace Silly_Holiday
 
         double test;
 
-        int xRocket = 52;
-        int yRocket = 330;
-        int hRocket = 30;
-        int wRocket = 30;
-
-        int wRocketBody = 60;
-        int hRocketBody = 120;
-        int SideRocketBody = 42;
-
+        Random randGen = new Random();
+        int randValue;
+        double randValueD;
+        #endregion
+        #region Pens
         Graphics g;
         Pen blackPen = new Pen(Color.Black, 10);
         Pen grayPen = new Pen(Color.Gray, 10);
         Pen redPen = new Pen(Color.DarkOrange, 10);
+        Pen oceanPen = new Pen(Color.Blue, 10);
+        Pen landPen = new Pen(Color.DarkOliveGreen, 10);
         Pen defaultPen = new Pen(Color.FromArgb(30, 30, 30), 10);
+        Pen smokePen = new Pen(Color.FromArgb(120, 120, 120), 10);
         Pen darkGrayPen = new Pen(Color.FromArgb(90, 90, 90), 10);
-
-        Random randGen = new Random();
-        int randValue;
-        double randValueD;
 
         SolidBrush grayBrush = new SolidBrush(Color.Gray);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
         SolidBrush redBrush = new SolidBrush(Color.DarkOrange);
+        SolidBrush oceanBrush = new SolidBrush(Color.Blue);
+        SolidBrush landBrush = new SolidBrush(Color.DarkOliveGreen);
         SolidBrush defaultBrush = new SolidBrush(Color.FromArgb(30, 30, 30));
+        SolidBrush smokeBrush = new SolidBrush(Color.FromArgb(120, 120, 120));
         SolidBrush darkGrayBrush = new SolidBrush(Color.FromArgb(90, 90, 90));
+
+        Font drawFont = new Font("Arial", 35, FontStyle.Bold);
+        SolidBrush drawBrush = new SolidBrush(Color.DarkOrange);
+        #endregion
         public Form1()
         {
             InitializeComponent();
@@ -64,27 +68,35 @@ namespace Silly_Holiday
         }
         private void Form1_Shown(object sender, EventArgs e)
         {
-           // this.Size = new System.Drawing.Size(500, 600);
-           
-          //while (angle != 20)
-          for (int i = 0; i < 1; i++) 
-           {
-                Stars();
-                g.DrawRectangle(darkGrayPen, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
-                g.FillRectangle(darkGrayBrush, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
-                g.DrawEllipse(darkGrayPen, xRocket - 15, yRocket - (wRocketBody / 2 ), wRocketBody, wRocketBody);
-                g.FillEllipse(darkGrayBrush, xRocket - 15, yRocket - (wRocketBody / 2 ), wRocketBody, wRocketBody);
-                g.DrawEllipse(redPen, xRocket, yRocket, wRocket, hRocket);
-                g.FillEllipse(redBrush, xRocket, yRocket, wRocket, hRocket);
-                Thread.Sleep(200);
-                Rocket();
-                this.Close();
-            }
+            SoundPlayer spaceMusic = new SoundPlayer(Properties.Resources.spaceMusic);
+            spaceMusic.Play();
+            g.DrawEllipse(oceanPen, 70, 30, 220, 220);
+            g.FillEllipse(oceanBrush, 70, 30, 220, 220);
+
+            g.DrawEllipse(landPen, 100, 70, 30, 30);
+            g.FillEllipse(landBrush, 100, 70, 30, 30);
+            g.DrawEllipse(landPen, 100, 70, 30, 30);
+            g.FillEllipse(landBrush, 200, 140, 70, 70);
+            g.DrawEllipse(landPen, 120, 80, 70, 30);
+            g.FillEllipse(landBrush, 120, 80, 70, 30);
+            g.DrawEllipse(landPen, 120, 90, 50, 70);
+            g.FillEllipse(landBrush, 120, 90, 50, 70);
+            g.DrawEllipse(landPen, 120, 90, 50, 70);
+            g.FillEllipse(landBrush, 120, 170, 50, 70);
+            g.DrawEllipse(landPen, 170, 60, 50, 40);
+            g.FillEllipse(landBrush, 170, 60, 50, 40);
+            g.DrawEllipse(landPen, 150, 80, 50, 50);
+            g.FillEllipse(landBrush, 150, 80, 50, 50);
+
+            g.DrawEllipse(landPen, 220, 90, 20, 20);
+            g.FillEllipse(landBrush, 220, 90, 20, 20);
+            g.DrawEllipse(landPen, 80, 120, 20, 20);
+            g.FillEllipse(landBrush, 80, 120, 20, 20);
+            g.DrawEllipse(landPen, 200, 220, 20, 20);
+            g.FillEllipse(landBrush, 200, 220, 20, 20);
         }
         void Stars()
         {
-           // g.DrawRectangle(grayPen, 0, 0, 800, 700);
-           // g.FillRectangle(grayBrush, 0, 0, 800, 700);
             for (int i = 1; i <= 5; i++)
             {
                 for (int k = 1; k <= 4; k++)
@@ -135,27 +147,102 @@ namespace Silly_Holiday
         }
         void Rocket()
         {
-            for (xRocket = 52; xRocket < 250; xRocket= xRocket + 2)
-            {
-                g.TranslateTransform(xRocket - 15, yRocket -80);
-                g.DrawEllipse(darkGrayPen, xRocket - 15, yRocket - 80,wRocketBody, wRocketBody);
-                g.FillEllipse(darkGrayBrush, xRocket - 15, yRocket - 80, wRocketBody, wRocketBody);
-                g.DrawRectangle(darkGrayPen, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
-                g.FillRectangle(darkGrayBrush, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
-                g.DrawEllipse(redPen, xRocket, yRocket, wRocket, hRocket);
-                g.FillEllipse(redBrush, xRocket, yRocket, wRocket, hRocket);
-                Thread.Sleep(20);
-                g.DrawRectangle(defaultPen, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
-                g.FillRectangle(defaultBrush, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
+            int xRocket = 52;
+            int yRocket = 330;
+            int hRocket = 30;
+            int wRocket = 30;
 
-                test =  Math.Pow((xRocket - 120),2)* .014 + 150;
-                yRocket = Convert.ToInt32(test);
-            }
-            g.DrawRectangle(darkGrayPen, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
-            g.FillRectangle(darkGrayBrush, xRocket - 15, yRocket - 80, wRocketBody, hRocketBody);
+            int wRocketBody = 60;
+            int hRocketBody = 120;
+            int xRocketBody = xRocket - 15;
+            int yRocketBody = yRocket - 80;
+
+            int yRoof = yRocketBody - (wRocketBody / 2);
+            int xRoof = xRocket -16;
+            int lFlame = 40;
+            int wFlame = wRocketBody - 20;
+            int xFlame = xRocket - 5;
+
+            SoundPlayer launchSound = new SoundPlayer(Properties.Resources.rocket_launch_306441);
+            launchSound.Play();
+
+            g.DrawRectangle(darkGrayPen, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+            g.FillRectangle(darkGrayBrush, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+
+            g.DrawEllipse(darkGrayPen, xRoof, yRoof, wRocketBody, wRocketBody);
+            g.FillEllipse(darkGrayBrush, xRoof, yRoof, wRocketBody, wRocketBody);
+
             g.DrawEllipse(redPen, xRocket, yRocket, wRocket, hRocket);
             g.FillEllipse(redBrush, xRocket, yRocket, wRocket, hRocket);
-            Thread.Sleep(3000);
+            Thread.Sleep(200);
+
+            for (xRocket = 52; xRocket < 250; xRocket = xRocket + 2)
+            {
+              if (lFlame < 120)
+                 {
+                    lFlame = lFlame + 20;
+                 }
+                 else
+                  {
+                    lFlame = 40;
+                  }
+                    g.DrawEllipse(redPen, xFlame, yRocket, wFlame, lFlame);
+                    g.FillEllipse(redBrush, xFlame, yRocket, wFlame, lFlame);
+
+                    g.DrawEllipse(darkGrayPen, xRoof, yRoof, wRocketBody, wRocketBody);
+                    g.FillEllipse(darkGrayBrush, xRoof, yRoof, wRocketBody, wRocketBody);
+
+                    g.DrawEllipse(darkGrayPen, xRocketBody, yRocketBody, wRocketBody, wRocketBody);
+                    g.DrawEllipse(darkGrayPen, xRocketBody, yRocketBody, wRocketBody, wRocketBody);
+                    g.FillEllipse(darkGrayBrush, xRocketBody, yRocketBody, wRocketBody, wRocketBody);
+                    g.DrawRectangle(darkGrayPen, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+                    g.FillRectangle(darkGrayBrush, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+                    g.DrawEllipse(redPen, xRocket, yRocket, wRocket, hRocket);
+                    g.FillEllipse(redBrush, xRocket, yRocket, wRocket, hRocket);
+
+                    Thread.Sleep(20);
+                    g.DrawRectangle(defaultPen, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+                    g.FillRectangle(defaultBrush, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+                    g.DrawEllipse(defaultPen, xRoof, yRoof, wRocketBody, wRocketBody);
+                    g.FillEllipse(defaultBrush, xRoof, yRoof, wRocketBody, wRocketBody);
+                    g.DrawEllipse(defaultPen, xFlame, yRocket, wFlame, lFlame);
+                    g.FillEllipse(defaultBrush, xFlame, yRocket, wFlame, lFlame);
+
+                g.DrawEllipse(grayPen, 80, 300, 880, 880);
+                g.FillEllipse(grayBrush, 80, 300, 880, 880);
+
+                test = Math.Pow((xRocket - 120), 2) * .014 + 150;
+                    yRocket = Convert.ToInt32(test);
+
+                xRocketBody = xRocket - 15;
+                yRocketBody = yRocket - 80;
+                yRoof = yRocketBody - (wRocketBody / 2);
+                xRoof = xRocket - 16;
+                xFlame = xRocket - 5;
+            }
+            g.DrawEllipse(grayPen, 80, 300, 880, 880);
+            g.FillEllipse(grayBrush, 80, 300, 880, 880);
+            g.DrawRectangle(darkGrayPen, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+            g.FillRectangle(darkGrayBrush, xRocketBody, yRocketBody, wRocketBody, hRocketBody);
+            g.DrawEllipse(darkGrayPen, xRoof, yRoof, wRocketBody, wRocketBody);
+            g.FillEllipse(darkGrayBrush, xRoof, yRoof, wRocketBody, wRocketBody);
+            g.DrawEllipse(redPen, xRocket, yRocket, wRocket, hRocket);
+            g.FillEllipse(redBrush, xRocket, yRocket, wRocket, hRocket);
+            g.DrawString("You're out \n of this \n world!", drawFont, drawBrush, 50, 60);
+        }
+
+        private void cardButton_Click(object sender, EventArgs e)
+        {
+            g.DrawEllipse(defaultPen, 70, 30, 220, 220);
+            g.FillEllipse(defaultBrush, 70, 30, 220, 220);
+            cardButton.Hide();
+            for (int i = 0; i < 1; i++)
+            {
+                Stars();
+                Rocket();
+                SoundPlayer successMusic = new SoundPlayer(Properties.Resources.successMusic);
+                successMusic.Play();
+            }
         }
     }
 }
